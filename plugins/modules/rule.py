e4d7f67e221cbaca0f8071a322e55fbdbaf4d4ad
Michael Sekania <michael.sekania@rz.uni-augsburg.de>
2024-05-06 22:57:33 +0200

    test: 11

--- plugins/modules/rule.py
+++ plugins/modules/rule.py
@@ -644,13 +644,15 @@ class RuleAPI(CheckmkAPI):
             if r["extensions"]["conditions"] == desired["rule"]["conditions"]:
                 a += 2
 
-            if r["extensions"]["properties"] == desired_properties:
-                a += 4
-
             if self._raw_value_eval("search", r["extensions"]) == self._raw_value_eval(
                 "desired", desired["rule"]
             ):
                 a += 8
+
+            if r["extensions"]["properties"] == desired_properties:
+                a += 4
+            elif a == 11:
+                self.module.fail_json(msg=str(r["extensions"]["properties"]))
 
             if (
                 r["extensions"]["folder"] == desired["rule"]["location"]["folder"]
